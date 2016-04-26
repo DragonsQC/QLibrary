14a3864af75d208045bf14eca2038d04a6a08c16
DragonsQC <tkzl@vip.qq.com>
2016-04-26 16:01:47 +0800

    update

---
+++ library/src/main/java/com/dqc/qlibrary/library/dragindicator/PointEvaluator.java
@@ -0,0 +1,17 @@
+package com.dqc.qlibrary.library.dragindicator;
+
+import android.animation.TypeEvaluator;
+
+/**
+ * Created by panyi on 16/4/5.
+ */
+public class PointEvaluator implements TypeEvaluator<Point> {
+
+    @Override
+    public Point evaluate(float fraction, Point startValue, Point endValue) {
+        float x = startValue.getX() + fraction * (endValue.getX() - startValue.getX());
+        float y = startValue.getY() + fraction * (endValue.getY() - startValue.getY());
+        Point point = new Point(x, y);
+        return point;
+    }
+}//end class
