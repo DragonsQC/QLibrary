da18d87dac75d062def4d81dd710776e146ae5ea
QC Dragons <tkzl@vip.qq.com>
2016-07-24 00:49:38 +0800

    update

--- library/src/main/java/com/dqc/qlibrary/utils/ResourceUtil.java
+++ library/src/main/java/com/dqc/qlibrary/utils/ResourceUtil.java
@@ -4,6 +4,14 @@ import android.content.Context;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.RadialGradient;
+import android.graphics.Rect;
+import android.graphics.Shader;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.provider.MediaStore;
@@ -196,5 +204,77 @@ public class ResourceUtil {
     public static float getDensity(Context context) {
         initDisplayMetrics(context.getApplicationContext());
         return sDisplayMetrics.density;
+    }
+
+
+    /**
+     * 创建 圆形图片
+     *
+     * @param bitmap
+     * @return
+     */
+    public static Bitmap createRoundBitmap(Bitmap bitmap) {
+        int    size   = bitmap.getWidth() < bitmap.getHeight() ? bitmap.getWidth() : bitmap.getHeight();
+        Bitmap output = Bitmap.createBitmap(size, size, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(output);
+
+        final int   color   = 0xff424242;
+        final Paint paint   = new Paint();
+        final Rect  rect    = new Rect(0, 0, size, size);
+        final float roundPx = size / 2;
+
+        paint.setAntiAlias(true);
+        canvas.drawARGB(0, 0, 0, 0);
+        paint.setColor(color);
+        canvas.drawCircle(roundPx, roundPx, roundPx, paint);
+
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+        canvas.drawBitmap(bitmap, rect, rect, paint);
+
+        return output;
+    }
+
+    /**
+     * 创建 圆形图片 带白色边框
+     *
+     * @param bitmap
+     * @return
+     */
+    public static Bitmap createRoundBitmapWithWhiteBorder(Bitmap bitmap) {
+        bitmap = createRoundBitmap(bitmap);
+        return addRoundeBitmapWhiteBorder(bitmap);
+    }
+
+
+    /**
+     * 给 圆形图 添加 白色边框
+     *
+     * @param bitmap
+     * @return
+     */
+    public static Bitmap addRoundeBitmapWhiteBorder(Bitmap bitmap) {
+        int    size    = bitmap.getWidth() < bitmap.getHeight() ? bitmap.getWidth() : bitmap.getHeight();
+        int    num     = 14;
+        int    sizebig = size + num;
+        Bitmap output  = Bitmap.createBitmap(sizebig, sizebig, Bitmap.Config.ARGB_8888);
+        Canvas canvas  = new Canvas(output);
+
+        final int   color   = Color.parseColor("#FFFFFF");
+        final Paint paint   = new Paint();
+        final float roundPx = sizebig / 2;
+
+        paint.setAntiAlias(true);
+        canvas.drawARGB(0, 0, 0, 0);
+        paint.setColor(color);
+        canvas.drawBitmap(bitmap, num / 2, num / 2, paint);
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.DST_ATOP));
+
+        RadialGradient gradient = new RadialGradient(roundPx, roundPx, roundPx,
+                new int[]{Color.WHITE, Color.WHITE, Color.parseColor("#AAAAAAAA")},
+                new float[]{0.f, 0.97f, 1.0f}, Shader.TileMode.CLAMP);
+        paint.setShader(gradient);
+        canvas.drawCircle(roundPx, roundPx, roundPx, paint);
+
+        return output;
     }
 }
