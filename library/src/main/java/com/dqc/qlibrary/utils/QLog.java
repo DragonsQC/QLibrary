99562daa9c21c310898aa0a507f34cb8a08225fb
QC Dragons <tkzl@vip.qq.com>
2016-08-05 23:11:55 +0800

    update

--- library/src/main/java/com/dqc/qlibrary/utils/QLog.java
+++ library/src/main/java/com/dqc/qlibrary/utils/QLog.java
@@ -1,85 +1,121 @@
 package com.dqc.qlibrary.utils;
 
-import com.apkfuns.logutils.LogUtils;
+import com.orhanobut.logger.BuildConfig;
+import com.orhanobut.logger.Logger;
 
 /**
  * Android 日志工具类
+ * <p/>
+ * require https://github.com/orhanobut/logger
+ * <p/>
  *
  * @author DragonsQC
  */
 public class QLog {
 
-    public static void init(boolean configAllowLog, String configTagPrefix) {
-        LogUtils.getLogConfig()
-                .configAllowLog(configAllowLog)
-                .configTagPrefix(configTagPrefix)
-                .configShowBorders(true);
+    public static void init(String tag) {
+        Logger
+                .init(tag);
     }
 
     /**
-     * 日志工具
-     * <p/>
      * verbose 信息
      *
-     * @param o
+     * @param msg
      */
-    public static void v(Object o) {
-        LogUtils.v(o);
+    public static void v(String msg) {
+        if (BuildConfig.DEBUG) {
+            Logger.v(msg);
+        }
     }
 
     /**
-     * 日志工具
-     * <p/>
      * debug 调试信息
      *
      * @param o
      */
     public static void d(Object o) {
-        LogUtils.d(o);
+        if (BuildConfig.DEBUG) {
+            Logger.d(o);
+        }
     }
 
     /**
-     * 日志工具
-     * <p/>
      * info 普通信息
      *
-     * @param o
+     * @param msg
      */
-    public static void i(Object o) {
-        LogUtils.i(o);
+    public static void i(String msg) {
+        if (BuildConfig.DEBUG) {
+            Logger.i(msg);
+        }
     }
 
     /**
-     * 日志工具
-     * <p/>
      * warning 警告信息
      *
-     * @param o
+     * @param msg
      */
-    public static void w(Object o) {
-        LogUtils.w(o);
+    public static void w(String msg) {
+        if (BuildConfig.DEBUG) {
+            Logger.w(msg);
+        }
     }
 
     /**
-     * 日志工具
-     * <p/>
      * error 错误信息
      *
-     * @param o
+     * @param msg
      */
-    public static void e(Object o) {
-        LogUtils.e(o);
+    public static void e(String msg) {
+        if (BuildConfig.DEBUG) {
+            Logger.e(msg);
+        }
     }
 
     /**
-     * 日志工具
-     * <p/>
-     * json格式字符串
+     * error 错误信息
+     *
+     * @param msg
+     */
+    public static void e(Throwable throwable, String msg) {
+        if (BuildConfig.DEBUG) {
+            Logger.e(throwable, msg);
+        }
+    }
+
+    /**
+     * wtf
+     *
+     * @param msg
+     */
+    public static void wtf(String msg) {
+        if (BuildConfig.DEBUG) {
+            Logger.wtf(msg);
+        }
+    }
+
+
+    /**
+     * json 格式字符串
      *
      * @param json
      */
     public static void json(String json) {
-        LogUtils.json(json);
+        if (BuildConfig.DEBUG) {
+            Logger.json(json);
+        }
+    }
+
+    /**
+     * xml 格式
+     *
+     * @param xml
+     */
+    public static void xml(String xml) {
+        if (BuildConfig.DEBUG) {
+            Logger.xml(xml);
+        }
     }
 
 }
