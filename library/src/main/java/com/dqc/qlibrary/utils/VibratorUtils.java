17132418e9bea660d3b805a8744380a10d12e9c6
QC Dragons <tkzl@vip.qq.com>
2016-10-13 21:27:43 +0800

    update

--- library/src/main/java/com/dqc/qlibrary/utils/VibratorUtils.java
+++ library/src/main/java/com/dqc/qlibrary/utils/VibratorUtils.java
@@ -13,7 +13,7 @@ public class VibratorUtils {
      * @param activity
      * @param milliseconds 震动的时长，单位是毫秒
      */
-    public static void Vibrate(final Activity activity, long milliseconds) {
+    public static void vibrate(final Activity activity, long milliseconds) {
         Vibrator vib = (Vibrator) activity.getSystemService(Service.VIBRATOR_SERVICE);
         vib.vibrate(milliseconds);
     }
@@ -23,7 +23,7 @@ public class VibratorUtils {
      * @param pattern  自定义震动模式 。数组中数字的含义依次是[静止时长，震动时长，静止时长，震动时长。。。]时长的单位是毫秒
      * @param isRepeat 是否反复震动，如果是true，反复震动，如果是false，只震动一次
      */
-    public static void Vibrate(final Activity activity, long[] pattern, boolean isRepeat) {
+    public static void vibrate(final Activity activity, long[] pattern, boolean isRepeat) {
         Vibrator vib = (Vibrator) activity.getSystemService(Service.VIBRATOR_SERVICE);
         vib.vibrate(pattern, isRepeat ? 1 : -1);
     }
