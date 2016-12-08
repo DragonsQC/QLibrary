1a32920c21263761d158734551b782adf2812820
QC Dragons <tkzl@vip.qq.com>
2016-12-09 01:32:53 +0800

    update

--- library/src/main/java/com/dqc/qlibrary/utils/VibratorUtils.java
+++ library/src/main/java/com/dqc/qlibrary/utils/VibratorUtils.java
@@ -1,7 +1,7 @@
 package com.dqc.qlibrary.utils;
 
-import android.app.Activity;
 import android.app.Service;
+import android.content.Context;
 import android.os.Vibrator;
 
 /**
@@ -10,21 +10,21 @@ import android.os.Vibrator;
 public class VibratorUtils {
 
     /**
-     * @param activity
+     * @param context      Context
      * @param milliseconds 震动的时长，单位是毫秒
      */
-    public static void vibrate(final Activity activity, long milliseconds) {
-        Vibrator vib = (Vibrator) activity.getSystemService(Service.VIBRATOR_SERVICE);
+    public static void vibrate(final Context context, long milliseconds) {
+        Vibrator vib = (Vibrator) context.getSystemService(Service.VIBRATOR_SERVICE);
         vib.vibrate(milliseconds);
     }
 
     /**
-     * @param activity
+     * @param context  Context
      * @param pattern  自定义震动模式 。数组中数字的含义依次是[静止时长，震动时长，静止时长，震动时长。。。]时长的单位是毫秒
      * @param isRepeat 是否反复震动，如果是true，反复震动，如果是false，只震动一次
      */
-    public static void vibrate(final Activity activity, long[] pattern, boolean isRepeat) {
-        Vibrator vib = (Vibrator) activity.getSystemService(Service.VIBRATOR_SERVICE);
+    public static void vibrate(final Context context, long[] pattern, boolean isRepeat) {
+        Vibrator vib = (Vibrator) context.getSystemService(Service.VIBRATOR_SERVICE);
         vib.vibrate(pattern, isRepeat ? 1 : -1);
     }
 
