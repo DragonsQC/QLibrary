326e2dbc764a925d0d4423e24d659b93bd2b8247
DragonsQC <tkzl@vip.qq.com>
2017-06-01 23:28:14 +0800

    update

---
+++ library/src/main/java/com/dqc/qlibrary/utils/KeyboardUtils.java
@@ -0,0 +1,78 @@
+package com.dqc.qlibrary.utils;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+
+/**
+ * Android 键盘工具类
+ * <p>
+ * 避免输入法面板遮挡
+ * <p>
+ * 在manifest.xml中activity中设置<br>
+ * android:windowSoftInputMode="adjustPan"
+ */
+public class KeyboardUtils {
+
+    /**
+     * 显示虚拟键盘
+     *
+     * @param activity Activity
+     */
+    public static void showKeyboard(Activity activity) {
+        View view = activity.getCurrentFocus();
+        if (view == null) view = new View(activity);
+        InputMethodManager imm = (InputMethodManager) activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
+        if (imm == null) return;
+        imm.showSoftInput(view, InputMethodManager.SHOW_FORCED);
+    }
+
+    /**
+     * 显示虚拟键盘
+     *
+     * @param view View
+     */
+    public static void showKeyboard(View view) {
+        view.setFocusable(true);
+        view.setFocusableInTouchMode(true);
+        view.requestFocus();
+        InputMethodManager imm = (InputMethodManager) view.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+        if (imm == null) return;
+        imm.showSoftInput(view, InputMethodManager.SHOW_FORCED);
+    }
+
+    /**
+     * 隐藏虚拟键盘
+     *
+     * @param activity Activity
+     */
+    public static void hideKeyboard(Activity activity) {
+        View view = activity.getCurrentFocus();
+        if (view == null) view = new View(activity);
+        InputMethodManager imm = (InputMethodManager) activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
+        if (imm == null) return;
+        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+    }
+
+    /**
+     * 隐藏虚拟键盘
+     *
+     * @param view View
+     */
+    public static void hideKeyboard(View view) {
+        InputMethodManager imm = (InputMethodManager) view.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+        if (imm == null) return;
+        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+    }
+
+    /**
+     * 切换键盘显示与否状态
+     */
+    public static void toggleKeyboard(Context context) {
+        InputMethodManager imm = (InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE);
+        if (imm == null) return;
+        imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0);
+    }
+
+}
