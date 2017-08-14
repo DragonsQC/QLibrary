bb455c98f97b7bfe37039bac3d8a344cf0a08e10
DragonsQC <tkzl@vip.qq.com>
2017-08-14 20:37:24 +0800

    update

--- library/src/main/java/com/dqc/qlibrary/utils/FileUtils.java
+++ library/src/main/java/com/dqc/qlibrary/utils/FileUtils.java
@@ -128,24 +128,24 @@ public class FileUtils {
      * @param size 文件 length
      * @return 格式化后字符串
      */
-    public static String formatFileSize(long size) {
-        long kiloByte = size / 1024;
+    public static String formatFileSize(double size) {
+        double kiloByte = size / 1024;
         if (kiloByte < 1) {
             return size + "Byte";
         }
-        long megaByte = kiloByte / 1024;
+        double megaByte = kiloByte / 1024;
         if (megaByte < 1) {
             BigDecimal result1 = new BigDecimal(Double.toString(kiloByte));
             return result1.setScale(2, BigDecimal.ROUND_HALF_UP).toPlainString() + "KB";
         }
 
-        long gigaByte = megaByte / 1024;
+        double gigaByte = megaByte / 1024;
         if (gigaByte < 1) {
             BigDecimal result2 = new BigDecimal(Double.toString(megaByte));
             return result2.setScale(2, BigDecimal.ROUND_HALF_UP).toPlainString() + "MB";
         }
 
-        long teraBytes = gigaByte / 1024;
+        double teraBytes = gigaByte / 1024;
         if (teraBytes < 1) {
             BigDecimal result3 = new BigDecimal(Double.toString(gigaByte));
             return result3.setScale(2, BigDecimal.ROUND_HALF_UP).toPlainString() + "GB";
