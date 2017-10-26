b9724497a77adea929c71543e1ef0fddae8a854c
DragonsQC <tkzl@vip.qq.com>
2017-10-27 05:00:53 +0800

    update

---
+++ library/src/main/java/com/dqc/qlibrary/utils/QLibraryGlideModule.java
@@ -0,0 +1,13 @@
+package com.dqc.qlibrary.utils;
+
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.LibraryGlideModule;
+
+/**
+ * https://muyangmin.github.io/glide-docs-cn/doc/migrating.html#程序库
+ *
+ * @author DragonsQC
+ */
+@GlideModule
+public class QLibraryGlideModule extends LibraryGlideModule {
+}
