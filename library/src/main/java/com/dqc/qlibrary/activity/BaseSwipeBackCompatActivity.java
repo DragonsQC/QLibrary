953e31967af802c5239889e0e0d2d1607b2aa54d
DragonsQC <tkzl@vip.qq.com>
2017-12-25 02:12:15 +0800

    update

--- library/src/main/java/com/dqc/qlibrary/activity/BaseSwipeBackCompatActivity.java
+++ library/src/main/java/com/dqc/qlibrary/activity/BaseSwipeBackCompatActivity.java
@@ -1,11 +1,11 @@
 package com.dqc.qlibrary.activity;
 
 import android.os.Bundle;
+import android.support.v4.widget.SlidingPaneLayout;
+import android.view.View;
+import android.view.ViewGroup;
 
-import com.dqc.qlibrary.library.swipeback.SwipeBackActivityBase;
-import com.dqc.qlibrary.library.swipeback.SwipeBackActivityHelper;
-import com.dqc.qlibrary.library.swipeback.SwipeBackLayout;
-import com.dqc.qlibrary.library.swipeback.Utils;
+import java.lang.reflect.Field;
 
 /**
  * 侧滑返回 Activity
@@ -13,37 +13,42 @@ import com.dqc.qlibrary.library.swipeback.Utils;
  * @author .
  */
 @SuppressWarnings("WeakerAccess,unused")
-public abstract class BaseSwipeBackCompatActivity extends BaseAppCompatActivity implements SwipeBackActivityBase {
-    private SwipeBackActivityHelper mHelper;
+public abstract class BaseSwipeBackCompatActivity extends BaseAppCompatActivity implements SlidingPaneLayout.PanelSlideListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mHelper = new SwipeBackActivityHelper(this);
-        mHelper.onActivityCreate();
 
     }
 
-    @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-        mHelper.onPostCreate();
-    }
+    /**
+     * 初始化滑动返回
+     */
+    private void initSwipeBackFinish() {
+        SlidingPaneLayout slidingPaneLayout = new SlidingPaneLayout(this);
+        //通过反射改变mOverhangSize的值为0，这个mOverhangSize值为菜单到右边屏幕的最短距离，
+        //默认是32dp，现在给它改成0
+        try {
+            //属性
+            Field fOverHang = SlidingPaneLayout.class.getDeclaredField("mOverhangSize");
+            fOverHang.setAccessible(true);
+            fOverHang.set(slidingPaneLayout, 0);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        slidingPaneLayout.setPanelSlideListener(this);
+        slidingPaneLayout.setSliderFadeColor(getResources().getColor(android.R.color.transparent));
 
-    @Override
-    public SwipeBackLayout getSwipeBackLayout() {
-        return mHelper.getSwipeBackLayout();
-    }
+        View leftView = new View(this);
+        leftView.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+        slidingPaneLayout.addView(leftView, 0);
 
-    @Override
-    public void setSwipeBackEnable(boolean enable) {
-        getSwipeBackLayout().setEnableGesture(enable);
-    }
-
-    @Override
-    public void scrollToFinishActivity() {
-        Utils.convertActivityToTranslucent(this);
-        getSwipeBackLayout().scrollToFinishActivity();
+        ViewGroup decor      = (ViewGroup) getWindow().getDecorView();
+        ViewGroup decorChild = (ViewGroup) decor.getChildAt(0);
+        decorChild.setBackgroundColor(getResources().getColor(android.R.color.white));
+        decor.removeView(decorChild);
+        decor.addView(slidingPaneLayout);
+        slidingPaneLayout.addView(decorChild, 1);
     }
 
 }
