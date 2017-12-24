3bf9d76d898df960ca4059f7f445042587493c60
DragonsQC <tkzl@vip.qq.com>
2017-12-25 02:37:13 +0800

    update

--- library/src/main/java/com/dqc/qlibrary/activity/BaseAppCompatActivity.java
+++ library/src/main/java/com/dqc/qlibrary/activity/BaseAppCompatActivity.java
@@ -2,15 +2,10 @@ package com.dqc.qlibrary.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.widget.SlidingPaneLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.view.ViewGroup;
 
 import com.dqc.qlibrary.R;
 import com.jaeger.library.StatusBarUtil;
-
-import java.lang.reflect.Field;
 
 /**
  * BaseAppCompatActivity
@@ -18,7 +13,7 @@ import java.lang.reflect.Field;
  * @author .
  */
 @SuppressWarnings("WeakerAccess,unused")
-public abstract class BaseAppCompatActivity extends AppCompatActivity implements SlidingPaneLayout.PanelSlideListener {
+public abstract class BaseAppCompatActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,9 +40,6 @@ public abstract class BaseAppCompatActivity extends AppCompatActivity implements
                 default:
                     break;
             }
-        }
-        if (isSupportSwipeBack()) {
-            initSwipeBackFinish();
         }
         super.onCreate(savedInstanceState);
     }
@@ -79,45 +71,6 @@ public abstract class BaseAppCompatActivity extends AppCompatActivity implements
                     break;
             }
         }
-    }
-
-    /**
-     * 初始化滑动返回
-     */
-    private void initSwipeBackFinish() {
-        SlidingPaneLayout slidingPaneLayout = new SlidingPaneLayout(this);
-        //通过反射改变mOverhangSize的值为0，这个mOverhangSize值为菜单到右边屏幕的最短距离，
-        //默认是32dp，现在给它改成0
-        try {
-            //属性
-            Field fOverHang = SlidingPaneLayout.class.getDeclaredField("mOverhangSize");
-            fOverHang.setAccessible(true);
-            fOverHang.set(slidingPaneLayout, 0);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        slidingPaneLayout.setPanelSlideListener(this);
-        slidingPaneLayout.setSliderFadeColor(getResources().getColor(android.R.color.transparent));
-
-        View leftView = new View(this);
-        leftView.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
-        slidingPaneLayout.addView(leftView, 0);
-
-        ViewGroup decor      = (ViewGroup) getWindow().getDecorView();
-        ViewGroup decorChild = (ViewGroup) decor.getChildAt(0);
-        decorChild.setBackgroundColor(getResources().getColor(android.R.color.white));
-        decor.removeView(decorChild);
-        decor.addView(slidingPaneLayout);
-        slidingPaneLayout.addView(decorChild, 1);
-    }
-
-    /**
-     * 是否支持滑动返回
-     *
-     * @return boolean
-     */
-    protected boolean isSupportSwipeBack() {
-        return true;
     }
 
     /**
