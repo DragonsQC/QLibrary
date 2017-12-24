50e86287968c878fae5660931c446627c91bdbe0
DragonsQC <tkzl@vip.qq.com>
2017-12-25 02:20:26 +0800

    update

--- library/src/main/java/com/dqc/qlibrary/activity/BaseSwipeBackCompatActivity.java
+++ library/src/main/java/com/dqc/qlibrary/activity/BaseSwipeBackCompatActivity.java
@@ -17,6 +17,7 @@ public abstract class BaseSwipeBackCompatActivity extends BaseAppCompatActivity 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        initSwipeBackFinish();
         super.onCreate(savedInstanceState);
 
     }
