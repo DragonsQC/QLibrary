52f8a01351dabfa7a2cb3141afaee9ac03790cda
DragonsQC <tkzl@vip.qq.com>
2017-12-25 03:15:11 +0800

    update

--- library/src/main/java/com/dqc/qlibrary/activity/BaseSwipeBackCompatActivity.java
+++ library/src/main/java/com/dqc/qlibrary/activity/BaseSwipeBackCompatActivity.java
@@ -3,7 +3,7 @@ package com.dqc.qlibrary.activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 
-import com.dqc.qlibrary.library.swipeback.SwipeBackLayout;
+import com.aitangba.swipeback.SwipeBackActivity;
 
 /**
  * 侧滑返回 Activity
@@ -11,20 +11,16 @@ import com.dqc.qlibrary.library.swipeback.SwipeBackLayout;
  * @author .
  */
 @SuppressWarnings("WeakerAccess,unused")
-public abstract class BaseSwipeBackCompatActivity extends BaseAppCompatActivity {
-
-    private SwipeBackLayout mSwipeBackLayout;
+public abstract class BaseSwipeBackCompatActivity extends SwipeBackActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mSwipeBackLayout = new SwipeBackLayout(this);
     }
 
     @Override
     protected void onPostCreate(@Nullable Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
-        mSwipeBackLayout.attachActivity(this);
     }
 
 }
