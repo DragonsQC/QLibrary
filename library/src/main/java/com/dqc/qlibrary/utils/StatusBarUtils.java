0a150626204f84f2bdc16e74d83292262959cfc0
DragonsQC <tkzl@vip.qq.com>
2018-12-11 16:03:50 +0800

    update

---
+++ library/src/main/java/com/dqc/qlibrary/utils/StatusBarUtils.java
@@ -0,0 +1,29 @@
+package com.dqc.qlibrary.utils;
+
+import android.app.Activity;
+import android.os.Build;
+import android.view.View;
+
+import androidx.annotation.RequiresApi;
+
+/**
+ * 状态栏相关工具类
+ */
+public class StatusBarUtils {
+
+    /**
+     * 改变状态栏文字颜色（原生方式）
+     *
+     * @param activity Activity
+     * @param isDark   是否黑色文字，反之白色文字
+     */
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private static void setStatusBarTextColor(Activity activity, boolean isDark) {
+        View decor = activity.getWindow().getDecorView();
+        if (isDark) {
+            decor.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        } else {
+            decor.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+        }
+    }
+}
